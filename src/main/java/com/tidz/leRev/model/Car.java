f7a56c3b49403cce853a8b41d90db96078f208d0
Tiago Gomes <tidgomes@gmail.com>
2024-11-12 15:27:01 -0300

    create Car model and enum of car model types

---
+++ src/main/java/com/tidz/leRev/model/Car.java
@@ -0,0 +1,74 @@
+package com.tidz.leRev.model;
+
+import jakarta.persistence.*;
+
+import java.time.Year;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "model")
+    private CarModel model;
+    @Column(name = "year")
+    private Year year;
+    @Column(name = "color")
+    private String color;
+
+    public Car() {
+
+    }
+
+    public Car(String name, CarModel model, Year year, String color) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CarModel getModel() {
+        return model;
+    }
+
+    public void setModel(CarModel model) {
+        this.model = model;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/com/tidz/leRev/model/CarModel.java
@@ -0,0 +1,5 @@
+package com.tidz.leRev.model;
+
+public enum CarModel {
+    SEDAN, WAGON, HATCH, SPORT, COUPE, PICKUP, TRUCK, SUV, VAN;
+}
