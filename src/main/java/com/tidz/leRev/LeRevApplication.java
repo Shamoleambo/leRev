54b105558d51b109f20e154c3ff0329f851a3e30
Tiago Gomes <tidgomes@gmail.com>
2024-11-12 15:05:10 -0300

    first commitor

---
+++ src/main/java/com/tidz/leRev/LeRevApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.leRev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeRevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/leRev/LeRevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.leRev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
