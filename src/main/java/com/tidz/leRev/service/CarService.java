fa5facf93b14267ff483a54143c3cb5812618095
Tiago Gomes <tidgomes@gmail.com>
2024-11-12 15:59:39 -0300

    implement error case scenario where a car is not provided to be added

---
+++ src/main/java/com/tidz/leRev/service/CarService.java
@@ -0,0 +1,25 @@
+package com.tidz.leRev.service;
+
+import com.tidz.leRev.exception.EmptyResourceError;
+import com.tidz.leRev.model.Car;
+import com.tidz.leRev.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+
+    private CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car add(Car newCar) {
+        if (newCar == null) {
+            throw new EmptyResourceError("A car must be provided");
+        }
+        return null;
+    }
+}
