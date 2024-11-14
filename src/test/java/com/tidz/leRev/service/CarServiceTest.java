43dfec9f41a4220d8b5258750b7e55ac16af20e3
Tiago Gomes <tidgomes@gmail.com>
2024-11-14 16:18:11 -0300

    test: return the saved car

--- src/test/java/com/tidz/leRev/service/CarServiceTest.java
+++ src/test/java/com/tidz/leRev/service/CarServiceTest.java
@@ -3,6 +3,7 @@ package com.tidz.leRev.service;
 import com.tidz.leRev.exception.EmptyResourceError;
 import com.tidz.leRev.exception.InternalServerError;
 import com.tidz.leRev.model.Car;
+import com.tidz.leRev.model.CarModel;
 import com.tidz.leRev.repository.CarRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.Year;
 
 @SpringBootTest
 public class CarServiceTest {
@@ -46,5 +49,18 @@ public class CarServiceTest {
         Assertions.assertThrows(InternalServerError.class, () -> {
             service.add(car);
         });
+    }
+
+    @Test
+    void serviceShouldReturnACarWhenSuccess() {
+        Car car = new Car("Audi RS4", CarModel.SEDAN, Year.of(1996), "blue");
+
+        Mockito.when(repository.save(car)).thenReturn(car);
+        Car returnedCar = service.add(car);
+
+        Assertions.assertEquals(car.getName(), returnedCar.getName());
+        Assertions.assertEquals(car.getModel(), returnedCar.getModel());
+        Assertions.assertEquals(car.getYear(), returnedCar.getYear());
+        Assertions.assertEquals(car.getColor(), returnedCar.getColor());
     }
 }
